2bec2f5a6120abf51ae571f26739271857408c22
ChesterCheeto <calebjpilon@gmail.com>
2023-06-01 11:37:25 -0400

    Add files via upload

--- outlook broke fixer.bat
+++ outlook broke fixer.bat
@@ -2,9 +2,13 @@
 set file_path="%appdata%\Microsoft\outlook\OutlPrnt"
 set process_name="Outlook.exe"
 set outlook_location=" C:\Program Files\Microsoft Office\Office15\OUTLOOK.EXE"
-taskkill /im %process_name%
-echo save changes or whatever, close outlook then press any key
-pause
+FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %process_name%"') DO IF NOT %%x == %process_name% (
+  echo %process_name% is not running continuing..
+) else (
+    taskkill /im %process_name%
+    echo save changes or whatever, close outlook then press any key
+    pause
+)
 if exist %file_path% (
     echo Badboifile exists. Deleting...
     del %file_path%
@@ -12,5 +16,10 @@ if exist %file_path% (
 ) else (
     echo Badboifile not found something else is wrong dawg.
 )
-start  %outlook_location%
+if exist %file_path% (
+    start  %outlook_location%
+    echo starting outlook
+) else (
+    echo outlook path not found cannot start
+)
 pause
